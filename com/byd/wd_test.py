15483f5bd6938814dbd6df494f3de089d094b7b5
qt7mira <771307676@qq.com>
2018-01-08 18:27:34 +0800

    byd  维度匹配  modify  考虑分句

--- com/byd/wd_test.py
+++ com/byd/wd_test.py
@@ -2,14 +2,27 @@ import pandas as pd
 import numpy as np
 import jieba
 import datetime
+import re
 
 
 def str_dp(str1):
-    return list(str(str1).strip().split(' '))
+    return list(str(str1).strip().lower().split(' '))
 
 
 def str_dp2(str1):
-    return jieba.lcut(str(str1).strip())
+    return jieba.lcut(str(str1).lower().strip())
+
+
+def cut_short_sentences(data_list):
+    sentence_list = []
+    for data in data_list:
+        if len(data) > 30:
+            for s in re.split('。|！|？|；| |>|【|】| |；|：', data):
+                if len(s) != 0:
+                    sentence_list.append(s)
+        else:
+            sentence_list.append(data)
+    return sentence_list
 
 begin = datetime.datetime.now()
 jieba.load_userdict("C:/Users/Administrator/Desktop/aaa.txt")
@@ -18,11 +31,14 @@ wd = pd.read_excel("C:/Users/Administrator/Desktop/汽车维度整理20180105.xl
 wd_col_1 = wd['4rank']
 wd_col_2 = wd['desc']
 
-content = pd.read_excel("C:/Users/Administrator/Desktop/autohome_bbs_2017-08-07.xlsx")['G']
+content = np.array(pd.read_excel("C:/Users/Administrator/Desktop/autohome_bbs_2017-08-07.xlsx")['G'][0:100]).tolist()
+content_dp = cut_short_sentences(content)
 
-print(content[0:10])
+print(len(content_dp))
+
+print(content_dp[0:10])
 print(wd_col_2[0:10])
-content_cut = pd.Series(content).apply(str_dp2)
+content_cut = pd.Series(content_dp).apply(str_dp2)
 wd_col_2 = pd.Series(wd_col_2).apply(str_dp)
 print(len(content_cut))
 print(content_cut[0])
@@ -40,7 +56,7 @@ no_re_wd_words = set(no_re_wd_words)
 result = []
 for i in range(len(content_cut)):
     res = {}
-    res['desc'] = content[i]
+    res['desc'] = content_dp[i]
     res['desc_cut'] = content_cut[i]
     res['wd'] = "null"
     res['wd_desc'] = "null"
@@ -59,7 +75,8 @@ for i in range(len(content_cut)):
     result.append(res)
 
 result_df = pd.DataFrame(result)
-result_df.to_excel("C:/Users/Administrator/Desktop/result_2.xlsx")
+print(result_df)
+result_df.to_excel("C:/Users/Administrator/Desktop/result_test.xlsx")
 stop = datetime.datetime.now()
 print("匹配维度用时" + str(stop-step1))
 print("总共用时" + str(stop-begin))
