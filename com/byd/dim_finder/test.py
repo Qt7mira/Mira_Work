7b43ea0891972823e4b0909580e97061fa929109
qt7mira <771307676@qq.com>
2018-01-12 18:47:25 +0800

    byd  返回多维度

---
+++ com/byd/dim_finder/test.py
@@ -0,0 +1,6 @@
+def str_dp(str1):
+    return ["aaa", str1]
+
+d = ["a", "b"]
+s = [str_dp(line) for line in d]
+print(s)
--- com/byd/es_tsst.py
+++
@@ -1,4 +0,0 @@
-from elasticsearch import Elasticsearch
-es = Elasticsearch('101.201.252.151:9200')
-tt = es.get(index="radiott", doc_type="artiststt", id="AWBzSSGGQYJQovPXN9FN")
-print(tt)
